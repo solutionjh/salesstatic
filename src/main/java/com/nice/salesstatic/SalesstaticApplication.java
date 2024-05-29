5f0106880ae5c0815c06b0c72766b1607cd7f1d9
kjh <solutionjh@gmail.com>
2024-05-29 23:11:15 +0900

    Initail

---
+++ src/main/java/com/nice/salesstatic/SalesstaticApplication.java
@@ -0,0 +1,13 @@
+package com.nice.salesstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesstaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesstaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nice/salesstatic/SalesstaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.salesstatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesstaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
